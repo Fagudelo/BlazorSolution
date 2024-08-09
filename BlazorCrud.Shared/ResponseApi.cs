aaf4ccf67bb61d373f1f1b77af03c7ff28201a58
Fagudelo <ferney.certifications@outlook.com>
2024-08-09 15:17:37 -0500

    Se crean los modelos de datos

---
+++ BlazorCrud.Shared/ResponseApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Shared
+{
+    public class ResponseApi<T>
+    {
+        public bool EsCorrecto { get; set; }
+        public T? Valor { get; set; }
+        public string? Mensaje { get; set; }
+    }
+}
